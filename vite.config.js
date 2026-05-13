3afdc91492c0292475341d78c1093161f5c5d545
Manurach-T <manurach.tie@gmail.com>
2026-05-13 15:40:49 +0700

    chore: update base path and deploy config for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,6 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/workshop-design-system/pain-to-profit/',
+  base: '/pain-to-profit/',
   plugins: [react(), tailwindcss()],
 })
